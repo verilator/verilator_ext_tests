e48446533d652db4854af95b7a84b7e09d303924
Wilson Snyder <wsnyder@wsnyder.org>
2020-05-16 13:40:48 -0400

    Update subprojects; add SweRV-EH2/EL2.

---
+++ t/t_cores_eh2_cmark.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2003 by Wilson Snyder. This program is free software; you can
+# redistribute it and/or modify it under the terms of either the GNU
+# General Public License or the Perl Artistic License.
+
+use File::Spec;
+
+scenarios(vlt => 1);
+
+$ENV{RV_ROOT} = File::Spec->rel2abs($Self->{t_dir}."/../submodules/Cores-SweRV-EH2");
+$ENV{VERILATOR} = "$ENV{VERILATOR_ROOT}/bin/verilator";
+
+# This will run the canned CoreMark (even if you have a riscv64-unknown-elf
+# toolchain on your path), from ICCM but otherwise using the default core
+# configuration. Running from ICCM is faster and hopefully more exciting.
+# Note the build happens in $Self->{obj_dir} as the SweRV build system can
+# find everything via RV_ROOT. This leaves the submodule clean.
+run(cmd => ["make -j4 -C $Self->{obj_dir} -f $ENV{RV_ROOT}/tools/Makefile",
+            "VERILATOR=$ENV{VERILATOR} CONF_PARAMS=-iccm_enable=1",
+            "GCC_PREFIX=none TEST=cmark_iccm",
+            "VERILATOR_MAKE_FLAGS=VM_PARALLEL_BUILDS=1 verilator"],
+    logfile => "$Self->{obj_dir}/sim.log",
+    );
+
+file_grep("$Self->{obj_dir}/sim.log", qr/.*\nTEST_PASSED\n/is);
+
+ok(1);
+1;
