e70ec9fcdfbe863cee8d7ca687a0a0c3dac80da3
Wilson Snyder <wsnyder@wsnyder.org>
2025-11-09 15:13:46 -0500

    Tests: Add t/t_uvm_hello_v2020_3_1_nodpi.py

---
+++ t/t_uvm_hello_v2020_3_1_nodpi.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# DESCRIPTION: Verilator: Verilog Test driver/expect definition
+#
+# Copyright 2024 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
+# Lesser General Public License Version 3 or the Perl Artistic License
+# Version 2.0.
+# SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
+
+import vltest_bootstrap
+
+test.scenarios('vlt')
+test.top_filename = "t/t_uvm_hello.v"
+
+uvm_git = os.path.abspath(test.t_dir + "/../submodules/uvm")
+uvm_root = os.path.abspath(test.obj_dir + "/uvm-worktree")
+
+if not os.path.exists(uvm_root):
+    test.run(cmd=[
+        "cd " + uvm_git +
+        # " && git fetch --tags"  # .gitmodules doesn't pull tags - now using branch
+        " && git worktree prune && git worktree add --detach " + uvm_root +
+        " origin/uvm-2020-3.1-vlt"
+    ])
+
+## Make uvm_pkg_all
+
+e_filename = test.obj_dir + "/pp__" + test.name + "__all1.vpp"
+
+test.run(
+    cmd=[
+        os.environ["VERILATOR_ROOT"] + "/bin/verilator",
+        "--E --preproc-defines --no-preproc-comments ",  #
+        "+define+UVM_NO_DPI",
+        "+incdir+" + uvm_root + "/src",
+        test.top_filename,
+        "> " + e_filename,
+    ],
+    verilator_run=True)
+
+packed_filename = test.obj_dir + "/pp__" + test.name + "__all2.vpp"
+
+test.run(cmd=[
+    os.environ["VERILATOR_ROOT"] + "/nodist/uvm_pkg_packer",
+    "--test-name " + test.name,
+    "--uvm-header-filename " + uvm_root + "/src/uvm_pkg.sv",  #
+    "< " + e_filename,
+    "> " + packed_filename,
+])
+
+test.copy_if_golden(
+    packed_filename, os.environ["VERILATOR_ROOT"] +
+    "/test_regress/t/uvm/uvm_pkg_all_v2020_3_1_nodpi.svh")
+
+## Test
+
+test.compile(
+    v_flags2=["+define+UVM_NO_DPI"],
+    verilator_flags2=[
+        "--binary -j 0 -Wall --dump-inputs",  #
+        "-Wno-EOFNEWLINE",  # Temp - need to cleanup UVM repo
+        "+incdir+" + uvm_root + "/src",
+    ])
+
+test.execute()
+
+test.file_grep(test.run_log_filename, r'Hello World')
+
+test.passes()
